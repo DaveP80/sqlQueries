ac2091acbb7af4e34b738712d9cb0b4baab25a55
DaveP80 <davidpaq1@gmail.com>
2023-06-03 23:50:22 -0400

    azure mock data

---
+++ azure/studentgrades.sql
@@ -0,0 +1,99 @@
+-- Microsoft SQL server
+create table Grades (
+	Student VARCHAR(9),
+	Subject VARCHAR(21),
+	Marks INT,
+	core VARCHAR(50)
+);
+insert into Grades (Student, Subject, Marks, core) values ('Matthew', 'Science', 98, 1);
+insert into Grades (Student, Subject, Marks, core) values ('Alexander', 'Social Studies', 53, 0);
+insert into Grades (Student, Subject, Marks, core) values ('Benjamin', 'Science', 42, 1);
+insert into Grades (Student, Subject, Marks, core) values ('Abigail', 'English Language Arts', 71, 1);
+insert into Grades (Student, Subject, Marks, core) values ('Michael', 'Social Studies', 99, 1);
+insert into Grades (Student, Subject, Marks, core) values ('Emily', 'Fine Arts', 51, 1);
+insert into Grades (Student, Subject, Marks, core) values ('Mia', 'Physical Education', 42, 0);
+insert into Grades (Student, Subject, Marks, core) values ('Evelyn', 'Fine Arts', 72, 1);
+insert into Grades (Student, Subject, Marks, core) values ('Daniel', 'Physical Education', 69, 0);
+insert into Grades (Student, Subject, Marks, core) values ('Emily', 'Physical Education', 80, 1);
+insert into Grades (Student, Subject, Marks, core) values ('James', 'Mathematics', 49, 1);
+insert into Grades (Student, Subject, Marks, core) values ('Daniel', 'Social Studies', 45, 1);
+insert into Grades (Student, Subject, Marks, core) values ('Ava', 'Science', 98, 1);
+insert into Grades (Student, Subject, Marks, core) values ('Lucas', 'Physical Education', 70, 1);
+insert into Grades (Student, Subject, Marks, core) values ('William', 'Mathematics', 57, 1);
+insert into Grades (Student, Subject, Marks, core) values ('Ava', 'Science', 85, 0);
+insert into Grades (Student, Subject, Marks, core) values ('Harper', 'Social Studies', 53, 0);
+insert into Grades (Student, Subject, Marks, core) values ('Isabella', 'Fine Arts', 89, 0);
+insert into Grades (Student, Subject, Marks, core) values ('Ethan', 'Science', 78, 0);
+insert into Grades (Student, Subject, Marks, core) values ('Henry', 'Mathematics', 57, 0);
+insert into Grades (Student, Subject, Marks, core) values ('Michael', 'Science', 62, 1);
+insert into Grades (Student, Subject, Marks, core) values ('Ethan', 'Mathematics', 65, 1);
+insert into Grades (Student, Subject, Marks, core) values ('William', 'Science', 52, 1);
+insert into Grades (Student, Subject, Marks, core) values ('Oliver', 'Mathematics', 66, 1);
+insert into Grades (Student, Subject, Marks, core) values ('Lucas', 'Social Studies', 50, 0);
+insert into Grades (Student, Subject, Marks, core) values ('Matthew', 'Mathematics', 92, 0);
+insert into Grades (Student, Subject, Marks, core) values ('James', 'Science', 47, 1);
+insert into Grades (Student, Subject, Marks, core) values ('William', 'Fine Arts', 59, 1);
+insert into Grades (Student, Subject, Marks, core) values ('Abigail', 'Social Studies', 57, 0);
+insert into Grades (Student, Subject, Marks, core) values ('Isabella', 'Physical Education', 56, 1);
+insert into Grades (Student, Subject, Marks, core) values ('Charlotte', 'Fine Arts', 47, 1);
+insert into Grades (Student, Subject, Marks, core) values ('Lucas', 'Mathematics', 40, 1);
+insert into Grades (Student, Subject, Marks, core) values ('Emma', 'English Language Arts', 49, 1);
+insert into Grades (Student, Subject, Marks, core) values ('Daniel', 'English Language Arts', 45, 0);
+insert into Grades (Student, Subject, Marks, core) values ('Charlotte', 'Mathematics', 98, 1);
+insert into Grades (Student, Subject, Marks, core) values ('William', 'Social Studies', 97, 1);
+insert into Grades (Student, Subject, Marks, core) values ('Emma', 'Mathematics', 99, 0);
+insert into Grades (Student, Subject, Marks, core) values ('Abigail', 'Fine Arts', 82, 0);
+insert into Grades (Student, Subject, Marks, core) values ('Ava', 'Social Studies', 63, 0);
+insert into Grades (Student, Subject, Marks, core) values ('Michael', 'Science', 69, 1);
+insert into Grades (Student, Subject, Marks, core) values ('Emily', 'Fine Arts', 46, 0);
+insert into Grades (Student, Subject, Marks, core) values ('Elizabeth', 'Mathematics', 100, 0);
+insert into Grades (Student, Subject, Marks, core) values ('Harper', 'Fine Arts', 96, 1);
+insert into Grades (Student, Subject, Marks, core) values ('Harper', 'Social Studies', 41, 1);
+insert into Grades (Student, Subject, Marks, core) values ('Ava', 'Social Studies', 42, 1);
+insert into Grades (Student, Subject, Marks, core) values ('Abigail', 'English Language Arts', 50, 1);
+insert into Grades (Student, Subject, Marks, core) values ('Alexander', 'Mathematics', 49, 0);
+insert into Grades (Student, Subject, Marks, core) values ('Michael', 'Mathematics', 41, 1);
+insert into Grades (Student, Subject, Marks, core) values ('Evelyn', 'Mathematics', 83, 0);
+insert into Grades (Student, Subject, Marks, core) values ('Sophia', 'Fine Arts', 59, 0);
+insert into Grades (Student, Subject, Marks, core) values ('Abigail', 'Social Studies', 65, 0);
+insert into Grades (Student, Subject, Marks, core) values ('Alexander', 'Physical Education', 93, 1);
+insert into Grades (Student, Subject, Marks, core) values ('Elizabeth', 'Fine Arts', 58, 1);
+insert into Grades (Student, Subject, Marks, core) values ('Sophia', 'Physical Education', 44, 1);
+insert into Grades (Student, Subject, Marks, core) values ('James', 'Physical Education', 45, 0);
+insert into Grades (Student, Subject, Marks, core) values ('Michael', 'Science', 41, 0);
+insert into Grades (Student, Subject, Marks, core) values ('Ava', 'Fine Arts', 66, 1);
+insert into Grades (Student, Subject, Marks, core) values ('Amelia', 'Fine Arts', 69, 1);
+insert into Grades (Student, Subject, Marks, core) values ('William', 'Mathematics', 83, 1);
+insert into Grades (Student, Subject, Marks, core) values ('Harper', 'Mathematics', 76, 1);
+insert into Grades (Student, Subject, Marks, core) values ('Mia', 'Social Studies', 96, 1);
+insert into Grades (Student, Subject, Marks, core) values ('Matthew', 'Fine Arts', 85, 0);
+insert into Grades (Student, Subject, Marks, core) values ('Elizabeth', 'Science', 74, 1);
+insert into Grades (Student, Subject, Marks, core) values ('James', 'Mathematics', 72, 0);
+insert into Grades (Student, Subject, Marks, core) values ('Emily', 'Fine Arts', 61, 1);
+insert into Grades (Student, Subject, Marks, core) values ('Charlotte', 'Science', 45, 1);
+insert into Grades (Student, Subject, Marks, core) values ('Mia', 'Social Studies', 40, 0);
+insert into Grades (Student, Subject, Marks, core) values ('Matthew', 'Mathematics', 79, 0);
+insert into Grades (Student, Subject, Marks, core) values ('Mia', 'Science', 44, 0);
+insert into Grades (Student, Subject, Marks, core) values ('Elizabeth', 'English Language Arts', 61, 1);
+insert into Grades (Student, Subject, Marks, core) values ('Ethan', 'Physical Education', 90, 0);
+insert into Grades (Student, Subject, Marks, core) values ('Ava', 'Science', 54, 1);
+insert into Grades (Student, Subject, Marks, core) values ('Noah', 'Science', 58, 1);
+insert into Grades (Student, Subject, Marks, core) values ('Noah', 'Social Studies', 99, 1);
+insert into Grades (Student, Subject, Marks, core) values ('Evelyn', 'Social Studies', 55, 0);
+insert into Grades (Student, Subject, Marks, core) values ('Matthew', 'Social Studies', 67, 0);
+insert into Grades (Student, Subject, Marks, core) values ('Emily', 'Fine Arts', 43, 0);
+insert into Grades (Student, Subject, Marks, core) values ('Mia', 'Physical Education', 86, 1);
+insert into Grades (Student, Subject, Marks, core) values ('Henry', 'Physical Education', 97, 1);
+insert into Grades (Student, Subject, Marks, core) values ('Alexander', 'English Language Arts', 81, 1);
+-- alter table make mathematics and Enlish Language Arts as core subjects(boolean)
+UPDATE grades
+SET core = 0
+WHERE Subject NOT IN('mathematics','English Language Arts');
+
+UPDATE grades
+SET core = 1
+WHERE Subject IN('mathematics','English Language Arts');
+
+SELECT * FROM grades ORDER BY student, subject;
+-- Partition column 'firstyear' if value is 1, Student is a firstyear, else not a first year
+SELECT *, MAX(CASE WHEN core=1 THEN 1 ELSE 0 END) OVER (PARTITION BY Student) firstyear FROM Grades;
