f765ecc9babadd383391c0db284e4cbbcb0ed4d5
DaveP80 <107325159+DaveP80@users.noreply.github.com>
2023-11-24 00:03:08 -0500

    Update restaurantgrowth.sql

--- leetcode/restaurantgrowth.sql
+++ leetcode/restaurantgrowth.sql
@@ -2,27 +2,35 @@
 -- MySQL
 -- You are the restaurant owner and you want to analyze a possible expansion (there will be at least one customer every day).
 -- Compute the moving average of how much the customer paid in a seven days window (i.e., current day + 6 days before). average_amount should be rounded to two decimal places.
-SELECT
-	dna,
-	TRANSLATE(dna,
-	'ATGC',
-	'TACG') AS res
-FROM
-	dnastrand;
-
-WITH cte AS (
-SELECT
-	DISTINCT visited_on
-FROM
+with cte as (
+select
+	distinct visited_on
+from
 	customer
-WHERE
-	visited_on >= DATE_ADD((SELECT min(visited_on) FROM customer), INTERVAL 6 DAY)
+where
+	visited_on >= DATE_ADD((
+	select
+		min(visited_on)
+	from
+		customer),
+	interval 6 day)
 )
-SELECT
-	c.visited_on,
-	ROUND((SELECT sum(amount) FROM customer cu WHERE DATE_ADD(cu.visited_on, INTERVAL 6 DAY) >= 
-c.visited_on AND cu.visited_on <= c.visited_on), 2) amount,
-	ROUND((SELECT sum(amount) FROM customer cs WHERE 
-DATE_ADD(cs.visited_on, INTERVAL 6 DAY) >= c.visited_on AND cs.visited_on <= c.visited_on)/ 7, 2) average_amount
-FROM
-	cte c;
+select
+	z.*,
+	round(z.amount / 7,
+	2) average_amount
+from
+	(
+	select
+		c.visited_on,
+		(
+		select
+			sum(amount)
+		from
+			customer cu
+		where
+			DATE_add(cu.visited_on,
+			interval 6 day) >= c.visited_on
+				and cu.visited_on <= c.visited_on) amount
+	from
+		cte c) z;
